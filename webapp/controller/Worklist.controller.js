861d71bca5ac376a1eec32fc8e4b25d22405399b
IngaMuse <ingamoshkareva@gmail.com>
2024-10-15 11:21:23 +0300

    feat: added filter plant text

--- webapp/controller/Worklist.controller.js
+++ webapp/controller/Worklist.controller.js
@@ -1,65 +1,74 @@
-sap.ui.define([
-		"zjblessons/Worklist/controller/BaseController",
-		"sap/ui/model/json/JSONModel",
-		"zjblessons/Worklist/model/formatter",
-		"sap/ui/model/Filter",
-		"sap/ui/model/FilterOperator"
-	], function (BaseController, JSONModel, formatter, Filter, FilterOperator) {
-		"use strict";
+sap.ui.define(
+  [
+    "zjblessons/Worklist/controller/BaseController",
+    "sap/ui/model/json/JSONModel",
+    "zjblessons/Worklist/model/formatter",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator",
+  ],
+  function (BaseController, JSONModel, formatter, Filter, FilterOperator) {
+    "use strict";
 
-		return BaseController.extend("zjblessons.Worklist.controller.Worklist", {
+    return BaseController.extend("zjblessons.Worklist.controller.Worklist", {
+      formatter: formatter,
 
-			formatter: formatter,
+      onInit: function () {
+        const oViewModel = new JSONModel({});
+        this.setModel(oViewModel, "worklistView");
+      },
 
-			onInit : function () {
-				const oViewModel = new JSONModel({
-				});
-				this.setModel(oViewModel, "worklistView");
-			},
+      onSearch(oEvent) {
+        const sValue = oEvent.getParameter("value");
+        this._searchHandler(sValue);
+      },
+      onLiveSearch(oEvent) {
+        const sValue = oEvent.getParameter("newValue");
+        this._searchHandler(sValue);
+      },
 
-			onSearch(oEvent) {
-				const sValue = oEvent.getParameter('value');
-				this._searchHandler(sValue);
-			},
-			onLiveSearch(oEvent) {
-				const sValue = oEvent.getParameter('newValue');
-				this._searchHandler(sValue);
-			},
+      _searchHandler(sValue) {
+        const oTable = this.getView().byId("table"),
+          oFilter = !!sValue.length
+            ? new Filter({
+                filters: [
+                  new Filter("DocumentNumber", FilterOperator.Contains, sValue),
+                  new Filter("PlantText", FilterOperator.EQ, sValue),
+                ],
+              })
+            : [];
+        oTable.getBinding("items").filter(oFilter);
+      },
 
-			_searchHandler(sValue) {
-				const oTable = this.getView().byId('table'),
-				oFilter = !!sValue.length ? [new Filter('DocumentNumber', FilterOperator.Contains, sValue)] : [];
-				oTable.getBinding('items').filter(oFilter);
-			},
+      onPress: function (oEvent) {
+        this._showObject(oEvent.getSource());
+      },
 
-			onPress : function (oEvent) {
-				this._showObject(oEvent.getSource());
-			},
+      onNavBack: function () {
+        history.go(-1);
+      },
 
-			onNavBack : function() {
-				history.go(-1);
-			},
+      onRefresh: function () {
+        var oTable = this.byId("table");
+        oTable.getBinding("items").refresh();
+      },
 
-			onRefresh : function () {
-				var oTable = this.byId("table");
-				oTable.getBinding("items").refresh();
-			},
+      _showObject: function (oItem) {
+        this.getRouter().navTo("object", {
+          objectId: oItem.getBindingContext().getProperty("HeaderID"),
+        });
+      },
 
-			_showObject : function (oItem) {
-				this.getRouter().navTo("object", {
-					objectId: oItem.getBindingContext().getProperty("HeaderID")
-				});
-			},
-
-			_applySearch: function(aTableSearchState) {
-				var oTable = this.byId("table"),
-					oViewModel = this.getModel("worklistView");
-				oTable.getBinding("items").filter(aTableSearchState, "Application");
-				if (aTableSearchState.length !== 0) {
-					oViewModel.setProperty("/tableNoDataText", this.getResourceBundle().getText("worklistNoDataWithSearchText"));
-				}
-			}
-
-		});
-	}
+      _applySearch: function (aTableSearchState) {
+        var oTable = this.byId("table"),
+          oViewModel = this.getModel("worklistView");
+        oTable.getBinding("items").filter(aTableSearchState, "Application");
+        if (aTableSearchState.length !== 0) {
+          oViewModel.setProperty(
+            "/tableNoDataText",
+            this.getResourceBundle().getText("worklistNoDataWithSearchText")
+          );
+        }
+      },
+    });
+  }
 );
